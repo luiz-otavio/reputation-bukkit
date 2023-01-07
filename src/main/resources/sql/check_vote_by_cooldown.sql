5caa3ed6239b34facb599470bd6d541d050042b6
luiz-otavio <luizfarias2006@hotmail.com>
2023-01-07 04:30:56 -0300

    chore(sql): add `target_uuid` column for checking

--- src/main/resources/sql/check_vote_by_cooldown.sql
+++ src/main/resources/sql/check_vote_by_cooldown.sql
@@ -1,1 +1,1 @@
-SELECT true AS vote_exists FROM reputation_votes WHERE uuid = ? AND created_at >= ?;
+SELECT true AS vote_exists FROM reputation_votes WHERE uuid = ? AND target_uuid = ? AND created_at >= ?;
