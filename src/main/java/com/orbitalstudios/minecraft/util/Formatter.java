b440c604317e189388699e525b24bd9872aa17e0
luiz-otavio <luizfarias2006@hotmail.com>
2023-01-18 13:54:34 -0300

    fix(bukkit): resolve missed Formatter

--- src/main/java/com/orbitalstudios/minecraft/util/Formatter.java
+++ src/main/java/com/orbitalstudios/minecraft/util/Formatter.java
@@ -14,9 +14,9 @@ public class Formatter {
             hours = minutes / 60;
 
         if (hours > 0) {
-            return String.format("%d hours, %d minutes and %d seconds", hours, minutes, seconds);
+            return String.format("%d hours, %d minutes and %d seconds", hours, minutes % 60, seconds % 60);
         } else if (minutes > 0) {
-            return String.format("%d minutes and %d seconds", minutes, seconds);
+            return String.format("%d minutes and %d seconds", minutes % 60, seconds % 60);
         } else {
             return String.format("%d seconds", seconds);
         }
